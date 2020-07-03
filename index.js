6af3aaa99e979885ea4f4a7c95008d5b55d39d45
Moa Granberg <moa.granberg@hotmail.com>
2020-07-03 20:48:16 +0200

    Once per minute, add current temp to db, on '/' get simple temp-change

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const path = require("path");
+const fetch = require("node-fetch");
+const { db } = require("./firebase.js");
+const app = express();
+
+const port = process.env.PORT || 3005;
+app.listen(port, () => console.log(`Server running at ${port}`));
+
+const getCurrentDegree = async () => {
+  try {
+    const response = await fetch(
+      "http://blynk-cloud.com/9324999f577f4417997a0b59170b980e/get/v5"
+    );
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const addCurrentDegree = async () => {
+  try {
+    const newDegree = await getCurrentDegree();
+    const obj = {
+      temp: newDegree[0],
+      time: new Date().getTime(),
+    };
+    await db.collection("degrees").doc().set(obj);
+  } catch (err) {
+    console.error(err);
+  }
+};
+setInterval(addCurrentDegree, 60000);
+
+const getLastDegrees = async () => {
+  try {
+    const snapshots = await db
+      .collection("degrees")
+      .orderBy("time", "desc")
+      .limit(5)
+      .get();
+    const degrees = [];
+    snapshots.forEach((doc) => degrees.push(doc.data()));
+
+    return degrees;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const calculateDegreeChange = async () => {
+  const degrees = await getLastDegrees();
+  for (let obj of degrees) {
+    if (obj.temp > degrees[0].temp) {
+      return "sinking";
+    }
+  }
+  return "rising";
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const change = await calculateDegreeChange();
+    res.status(200).send(change);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+});
