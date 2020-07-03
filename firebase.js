de8488da17be4a27a60db0bbd376a67cdb59d77a
Moa Granberg <moa.granberg@hotmail.com>
2020-07-03 21:44:42 +0200

    Serve serviceAccount trough dotenv

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccount");
+const serviceAccount = JSON.parse(process.env.SERVICEACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
